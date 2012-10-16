222af0c748c9a002fe40e083f37792f01aa5c6d1
Abby Pan <abbypan@gmail.com>
2012-10-16 20:47:04 +0800

    update

--- gen.sh
+++ gen.sh
@@ -6,7 +6,7 @@ message=$1
 
 git commit . -m "$message"
 
-git push origin master
+git push origin master --force
 
 cd ..
 lftp infonet -e "mirror -X '.git/*' --reverse --delete --only-newer --ignore-time --verbose abbypan.github.com public_html;exit"
