3dbc251373bd2612e9807bd99a5580028e596920
Abby Pan <abbypan@gmail.com>
2012-10-19 18:38:40 +0800

    blog

--- gen.sh
+++ gen.sh
@@ -8,5 +8,5 @@ git commit . -m "$message"
 
 git push origin master --force
 
-cd ..
-lftp infonet -e "mirror -X '.git/*' --reverse --delete --only-newer --ignore-time --verbose abbypan.github.com public_html;exit"
+#cd ..
+#lftp infonet -e "mirror -X '.git/*' --reverse --delete --only-newer --ignore-time --verbose abbypan.github.com public_html;exit"
