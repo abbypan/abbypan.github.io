9df9c61da19e1f1f94eaf4703290a8f6d1d8fc49
abbypan <abbypan@gmail.com>
2014-05-23 00:42:05 +0800

    music

--- music/music.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/perl
-use File::Slurp qw/slurp/;
-use Template;
-
-system("perl ../../github/baidu_music/baidu_music.pl -i music_id.txt -t online -f mp3 -o music.json");
-my $c = slurp('music.json');
-my $tt = Template->new();
-$tt->process('music.tt2', { data => $c }, 'music.html'); 
-unlink('music.json');
